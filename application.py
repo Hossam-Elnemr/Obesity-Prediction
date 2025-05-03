55e249e91080aabb106bdc60473b8370b4f24119
Hossam ElNemr <hossam.a.elnemr@gmail.com>
2025-05-03 04:45:37 +0300

    Update application.py

--- application.py
+++ application.py
@@ -1,2 +1,8 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import streamlit as st
+import sklearn as sk
+train_data = pd.read_csv('train_dataset.csv')
+test_data = pd.read_csv('test_dataset.csv')
+st.write(train_data)
