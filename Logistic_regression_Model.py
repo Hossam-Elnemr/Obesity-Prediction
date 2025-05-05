ff5152ec13caa17f0f19c935c532fc1f6d96024e
Hussein Yahia <hussien.elnemr@gmail.com>
2025-05-05 11:57:28 +0300

    one vs rest model created

--- Logistic_regression_Model.py
+++ Logistic_regression_Model.py
@@ -47,6 +47,39 @@ def gradient_descent(X,y , w_initial, b_initial, alpha, iterations):
                 print(f"Iteration {i:4d}: Cost {J_history[-1]}   ")
     return w, b
 
+def one_vs_rest_train(X, y, classes, alpha=0.01, iterations=1000):
+    classifiers = {}
+    m, n = X.shape
+    for cls in classes:
+        print(f"\nTraining classifier for class: {cls}")
+        # Convert y to binary: 1 if current class, 0 otherwise
+        y_binary =[]
+        for label in y:
+            if label == cls:
+                y_binary.append(1)
+            else:
+                y_binary.append(0)
 
-def predict(X, w, b):
-    return sigmoid(np.dot(X, w) + b) >= 0.5
+        w_init = np.zeros(n)
+        b_init = 0
+
+        # Train logistic regression
+        w_trained, b_trained = gradient_descent(X, y_binary, w_init, b_init, alpha, iterations)
+
+        # Save model
+        classifiers[cls] = (w_trained, b_trained)
+    
+    return classifiers
+
+def one_vs_rest_predict(X, classifiers):
+    predictions = []
+    m = X.shape[0]
+    for i in range(m):
+        class_probs = {}
+        for cls, (w, b) in classifiers.items():
+            prob = sigmoid(np.dot(X[i], w) + b)
+            class_probs[cls] = prob
+        # Choose the class with the highest probability
+        predicted_class = max(class_probs, key=class_probs.get)
+        predictions.append(predicted_class)
+    return predictions
