430b28612de69277935a12744b12f1758abdd69b
Hossam ElNemr <hossam.a.elnemr@gmail.com>
2025-05-05 04:43:28 +0300

    Update application.py

--- application.py
+++ application.py
@@ -4,6 +4,6 @@ import matplotlib.pyplot as plt
 import streamlit as st
 import sklearn as sk
 from Logistic_regression_Model import gradient_descent, predict
-train_data = pd.read_csv("Obesity-Prediction/train_dataset.csv")
-test_data = pd.read_csv("Obesity-Prediction/test_dataset.csv")
+train_data = pd.read_csv('train_dataset.csv')
+test_data = pd.read_csv('test_dataset.csv')
 st.write(train_data)
